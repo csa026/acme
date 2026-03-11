79ed2494cbc6807968468c40fa058c4b9557a3c9
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2026-03-11 22:19:05 +0800

    Update acme.sh

--- acme.sh
+++ acme.sh
@@ -246,7 +246,11 @@ fi
 
 ACMEstandaloneDNS(){
 v4v6
-readp "请输入解析完成的域名:" ym
+vpsip=${v4:-$v6}
+readp "请输入解析完成的域名 (回车跳过表示没有域名，使用nip.io后缀域名自动解析IP):" ym
+if [ -z "$ym"]; then
+case "$vpsip" in *:*) ym="${vpsip//:/-}.nip.io" ;; *) ym="${vpsip//./-}.nip.io" ;; esac
+fi
 green "已输入的域名:$ym" && sleep 1
 checkacmeca
 checkip
